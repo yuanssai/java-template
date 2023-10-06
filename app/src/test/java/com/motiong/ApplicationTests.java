89827ca7f5f131e10c1821586ce2dce0fc50a8c7
yuansai <yuanssai@outlook.com>
2023-10-06 22:31:01 +0800

    spring init

---
+++ app/src/test/java/com/motiong/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.motiong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
